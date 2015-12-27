8981118401ee53e9bcf1f4293eb71afec17d3860
bibi21000 <bibi21000@gmail.com>
2015-12-27 22:24:56 +0100

    update db packages

---
+++ tests/test_fake.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""Unittests for postgresql client.
+"""
+__license__ = """
+    This file is part of Janitoo.
+
+    Janitoo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Janitoo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Janitoo. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+__copyright__ = "Copyright © 2013-2014-2015 Sébastien GALLET aka bibi21000"
+
+import sys, os
+import time, datetime
+import unittest
+import threading
+import logging
+from pkg_resources import iter_entry_points
+
+from janitoo_nosetests import JNTTBase
+import janitoo_postgresql_client
+
+class Fake(JNTTBase):
+    """Fake test
+    """
+    
+    def test_001_fake(self):
+        self.assertTrue(True)
